c3d1a25a56e49362bbc6e9173094c4be8d80763c
Silvio <silvio.attolini@mail.polimi.it>
2021-03-24 21:11:59 +0100

    more methods

--- HelloWorld2.py
+++ HelloWorld2.py
@@ -1,4 +1,7 @@
-from bottle import run, route, template, request
+# import bottle
+# import bottle_sqlite
+from bottle import run, route, template, request, error, abort, install
+from bottle_sqlite import SQLitePlugin
 
 
 @route('/')  # decorator
@@ -26,6 +29,11 @@ def getter():
     return '<h1>Getter Page</h1>'
 
 
+@route('/poster', method='POST')  # when a request is received
+def poster():
+    return '<h1>Poster Page</h1>'
+
+
 # templates
 @route('/template')
 def index():
@@ -40,6 +48,50 @@ def q_request():
 
     return '<h1>The values of the parameters are ' + p1 + ' and ' + p2 + '</h1>'
     # http://127.0.0.1:8080/q_request?par1=ppp1&par2=ppp2
+
+
+# error handling
+@error(404)
+def not_found(my_error):
+    return '<h1>Content not found</h1>' + my_error
+
+
+# error 401 override
+@error(401)
+def not_allowed(my_error):
+    return '<h1>You are not allowed to view this page</h1>' + my_error
+
+
+@route('/not_allowed_page')
+def not_allowed_page():
+    abort(401)
+
+
+# sqlite install 1
+
+# app = bottle.Bottle()
+# plugin = bottle.ext.sqlite.Plugin(db_file='/tmp/test.db')
+# app.install(plugin)
+
+
+#@app.route('/show/:item')
+#def show(item, db):
+#    row = db.execute('SELECT * from items where name=?', item).fetchone()
+#   if row:
+#        return template('show_item', page=row)
+#    return bottle.HTTPError(404, "Page not found")
+#
+
+# sqlite install 2
+install(SQLitePlugin(dbfile=r'.\\inventory.db'))
+
+
+@route('/show/<device_name:string>')
+def show_device(db, device_name):
+    command = db.execute('SELECT * FROM device', device_name)  # dove cazzo è device
+    row = command.fetchone()
+    return template('{{row}}', row=row)
+
 
 if __name__ == '__main__':
     run(debug=True, reloader=True)
