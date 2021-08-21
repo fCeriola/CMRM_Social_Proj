40d0cdb5d0c74cfd82e9b237b7dc4772ff02d579
Federico Ceriola <federico.ceriola@mail.polimi.it>
2021-08-21 18:54:20 +0200

    Profile img
    Stored in storage and refURL in db.user

--- ACTAM/backend/backend/static/js/auth.js
+++ ACTAM/backend/backend/static/js/auth.js
@@ -9,8 +9,6 @@ auth.onAuthStateChanged(user => {
     }
 })
 
-
-
 //signup form
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -20,18 +18,29 @@ signupForm.addEventListener('submit', (e) => {
     const email = signupForm['signup-email'].value;
     const password = signupForm['signup-password'].value;
     const nickname = signupForm['signup-nickname'].value;
-    
+    // const uploadProfileImg = document.getElementById('profile-img');
 
+
+    
     //signup the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        return db.collection('users').doc(cred.user.uid).set({
-            nick: nickname
-          });
-        }).then(()=>{
-        const modal = document.querySelector('#modal-signup')
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
-    }).catch(err => { alert(err.message) })
+            var profileImg = document.querySelector('#profile-img').files[0];
+            const fileName = nickname+'_prof-img.png';
+            const metadata = {
+                contentType: fileName.type
+            }
+            mediaStorage.child(fileName).put(profileImg, metadata).then(() =>{ 
+                    mediaStorage.child(fileName).getDownloadURL().then((url)=>{ 
+                        return db.collection('users').doc(cred.user.uid).set({nick: nickname, img: url});
+                });
+            });
+            
+        
+    }).then(() => {
+            const modal = document.querySelector('#modal-signup')
+                M.Modal.getInstance(modal).close();
+                signupForm.reset();
+            }).catch(err => { alert(err.message) });
 });
 
 //logout
@@ -56,5 +65,4 @@ loginForm.addEventListener('submit', (e) => {
     }).catch(err => {
         alert(err.message)
     })
-
 })
--- ACTAM/backend/backend/static/js/user_account.js
+++ ACTAM/backend/backend/static/js/user_account.js
@@ -6,9 +6,9 @@ const setupInfo = (user) => {
             const html = `
               <p>Logged in as:</p>
               <p><big><b>${doc.data().nick}</b></big></p>
-              
             `;
         userData.innerHTML = html
+        document.getElementById('user-img').setAttribute('src', doc.data().img)
     });
     }else{
         userData.innerHTML = ''
@@ -30,3 +30,22 @@ mediaStorage.child('ncw.png').getDownloadURL().then((url) => {
     var img = document.getElementById('navbar-img');
     img.setAttribute('src', url)
 });
+
+// const uploadProfileImg = document.getElementById('profile-img');
+// const nickname = document.getElementById('signup-nickname');
+// const email = document.getElementById('signup-email');
+
+// uploadProfileImg.onclick = function(){
+    
+//     const fileName = email+'_'+nickname;
+//     const metadata = {
+//         contentType: nameOfFile.type
+//     }
+//     const task = mediaStorage.child(fileName).put(uploadProfileImg, metadata);
+
+//     mediaStorage.child(fileName).getDownloadURL().then((url) => {
+//         var imgURL = url
+//     });
+
+// }
+    
